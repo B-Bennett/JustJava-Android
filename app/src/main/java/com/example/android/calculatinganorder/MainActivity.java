2ca66aeb813a4a5d6d9a450994fd05c0fde239b6
BennettIronYard <bennettiy12@gmail.com>
2016-06-09 12:28:56 -0400

    Push receipt summary to email

--- app/src/main/java/com/example/android/calculatinganorder/MainActivity.java
+++ app/src/main/java/com/example/android/calculatinganorder/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.android.calculatinganorder;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -17,77 +23,135 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity == 100) {
+            return;
+        }
         quantity = quantity + 1;
 
         displayQuantity(quantity);
     }
+
     /**
      * This method is called when the minus button is clicked.
      */
 
     public void decrement(View view) {
+        if (quantity == 0) {
+            return;
+        }
         quantity = quantity - 1;
 
         displayQuantity(quantity);
     }
-        /**
-         * This method is called when the order button is clicked.
-         */
+
+    /**
+     * This method is called when the order button is clicked.
+     */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-//       String priceMessage = createOrderSummary(price);
-//        displayMessage(priceMessage);
-        displayMessage(createOrderSummary(price));
+        /**
+         * Find the user's Name
+         */
+        EditText nameField = (EditText) findViewById(R.id.name_field);
+        Editable nameEditable = nameField.getText();
+        String name = nameField.getText().toString();
+        /**
+         * Figure out if the user wants whipped cream topping
+         */
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        /**
+         * Figure out if teh user wants chocolate topping
+         */
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        // Calculate the price
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        // Display the order summary on the screen
+        String priceMessage = createOrderSummary(name, price, hasWhippedCream, hasChocolate);
+
+        // Use an intent to launch an email app.
+        // Send the order summary in the email body.
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.just_java) + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+        displayMessage(priceMessage);
 
     }
-
     /**
      * Create summary of the order
      *
+     * @param name
      * @param price
+     * @param addChocolate
+     * @param addWhippedCream
      * @return text summary
      */
-    private String createOrderSummary(int price){
-        String priceMessage = "Name: Java Man";
-        priceMessage += priceMessage + "\nQantity: " + quantity;
-        priceMessage += priceMessage + "\nTotal: $"+ price;
-        priceMessage += priceMessage + "\nThank You!";
+    private String createOrderSummary(String name, int price, boolean addWhippedCream,
+                                      boolean addChocolate) {
+        String priceMessage = getString(R.string.order_summary_name, name);
+        priceMessage += "\n" + getString(R.string.add_whipped_cream) + addWhippedCream;
+        priceMessage += "\n" + getString(R.string.add_chocolate) + addChocolate;
+        priceMessage += "\n" + getString(R.string.Quantity) + quantity;
+        priceMessage += "\n" + getString(R.string.total);
+        priceMessage += "\n" + getString(R.string.thank_you);
         return priceMessage;
-
     }
     /**
      * Calculates the price of the order based on the current quantity.
      *
+     * @param addWhippedCream if user wants whipped cream topping or not
+     * @param addChocolate    if the user wants chocolate topping or not
      * @return total price
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        // Price of 1 cup of coffee
+        int basePrice = 5;
+
+        // Add $1 if the user want to add whipped cream topping
+        if (addWhippedCream) {
+            basePrice = basePrice + 1;
+        }
+        // Add $2 if teh user wants to add chocolate topping
+        if (addChocolate) {
+            basePrice = basePrice + 2;
+        }
+        // Calculate the total order by multiplying by quantity
+        return quantity * basePrice;
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayQuantity(int number) {
+    private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+//    private void displayPrice(int number) {
+//        TextView priceTextView = (TextView) findViewById(
+//                R.id.price_text_view);
+//        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+//    }
+
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
